5c79743d839e637d7f3103ba6ef1bffc78965f6d
Rebel_Graffiti <jhonkenrick28@gmail.com>
2019-11-21 16:44:26 +0800

    hi there

--- src/templates/NewsList.vue
+++ src/templates/NewsList.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="w-full xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-full xs:w-full mt-20 inline-block card-section">
+	<div class="w-full xl:w-1/3 lg:w-1/3 lg:mt-10 md:w-1/3 sm:w-full mt-4 inline-block card-section">
       <div class="max-w-sm mx-auto rounded overflow-hidden shadow-lg sm:mx-auto card__item">
         <g-image class="w-full news__image" :src="post.image" quality="50" alt="News"/>
         <div class="px-6 py-4 bg__new">
