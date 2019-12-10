fa756263d9d5baabb86c7497c02ce9f6af569c63
jhon kenrick <jhonkenrick28@gmail.com>
2019-12-10 15:58:26 +0800

    push

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -21,25 +21,43 @@
     </div>
     <div class="flex flex-row mt-20">
       <div class="container mx-auto">
-        <div class="w-full">
+        <div class="w-full my-10">
           <h2 class="integrity">Integrity | Service | Merit</h2>
           <p class="integrity__desc mt-2">Our School is diverse and dynamic, and our students have the highest aspirations for their education and future livesss.</p>
         </div>
+        <div class="w-full my-10">
+          <h2 class="integrity">Hardwork</h2>
+          <p class="integrity__desc mt-2">To maximize the God-given gift to-do and act with efficiency.</p>
+        </div>
+        <div class="w-full my-10">
+          <h2 class="integrity">Discipline</h2>
+          <p class="integrity__desc mt-2">
+          Being free to innovate and explore within the boundaries of common good for the people and for the environment.</p>
+        </div>
+        <div class="w-full my-10">
+          <h2 class="integrity">Gratitude</h2>
+          <p class="integrity__desc mt-2">
+          Being thankful for the new knowledge and skill acquired from school, teacher, colleagues, and its benefactors.</p>
+        </div>
+        <hr>
       </div>
+
     </div>
+
    <div class="flex flex-row mt-20">
       <div class="container mx-auto">
-        <div class="w-full">
+        <div class="w-full my-10">
           <h2 class="integrity">Mission</h2>
-          <p class="integrity__desc mt-2">Our School is diverse and dynamic, and our students have the highest aspirations for their education and future lives.</p>
-          <p class="mission_list my-4 pl-4 mr-4">1. Accentuate learning, growth, perseverance and self-awareness as life-long values.</p>
-          <p class="mission_list my-4 pl-4 mr-4">2. Nurture creativity and originality as precursors for critical and reﬂective thought and action.</p>
-          <p class="mission_list my-4 pl-4 mr-4">3. Accentuate learning, growth, perseverance and self-awareness as life-long values.</p>
-          <p class="mission_list my-4 pl-4 mr-4">4. Accentuate learning, growth, perseverance and self-awareness as life-long values.</p>
+          <p class="integrity__desc mt-2">CPI is committed to promote a quality education and develop a globally competitive individual by giving them a solid training ground and good exposure to practical application while nurturing values formation among the young people of Masbate.</p>
+        </div>
+         <div class="w-full my-10">
+          <h2 class="integrity">Vision</h2>
+          <p class="integrity__desc mt-2">CPI dreamed of delivering a quality education to the underprivileged youth of Masbate who have high potential in specialized professional field, thereby making them an all inclusive capable individual in various area bygiving them innovative culture and technology rich opportunities to address the difficulties of society today and in the future.</p>
+         
         </div>
         <h2 class="text-center history__title mt-20 mb-6">History</h2>
-        <p class="history__desc mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         <p class="history__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="history__desc mb-6">Cataingan Polytechnic Institute started out as Cataingan Computer Tutorial School on October 2009. Its primary reason is to introduce to the locale of Cataingan the usefulness of computer of their every day jobs as well as provide transient task to the technology experts who suffered job loss due to the defunct of a computer college. The first few clients of the group had been the lecturers who were then eager to get helped by means of the computer. Followed through the youths who are willing to get educated and to be computer literate to have an edge in looking for a job. It changed well as time goes by when the small commercial enterprise risked to bid for a slot in the first automated electoral vote casting. The school were given its slot and trained a number of youth.</p>
+         <p class="history__desc">There it begin the journey of Cataingan Polytechnic Institute. CPI officially applied for accreditation under the TESDA and was conceded to offer Computer Hardware Servicing NC II, Bread and Pastry NC II, Consumer Electronics Servicing NC II, Food and Beverage NC II and Housekeeping NC II. Additional TESDA courses was likewise offered, such as Hilot Wellness NC II, Bookkeeping NC III and Driving NC II. In 2015, it was granted an approval from DepEd to offer a Senior High School offering a Technical-Vocational-Livelihood track. A year after it was given grant from CHED to offer a multi year course BTBted, which give the understudy a choice to work as an educator or an employee in hospitality industry. Currently, CPI is working to expand and on its way to offer more courses that is in line with skills development.</p>
         <g-image src="../../static/about_bg_2.png" class="about_bg_2 my-10" alt="Admission" quality="60"/>
       </div>
     </div>
